97067b96066eeeaf92b53d7d4cad892eefc4e5df
mstutzman17 <mstutzman17@gmail.com>
2025-02-20 23:28:16 -0500

    added notes to notes file in 11.3

--- Unit_11-Intermediate_JavaScript/11.3-Sorting_Algorithms/dsa-sorting-demo/practice.js
+++ Unit_11-Intermediate_JavaScript/11.3-Sorting_Algorithms/dsa-sorting-demo/practice.js
@@ -43,3 +43,67 @@ function bubbleSort2(arr){
     console.log ("TOTAL COUNT:", count)
     return arr;
 }
+
+// function merge (arr1, arr2){
+//     const results = []
+//     let i = 0;
+//     let j = 0;    
+//     while (i < arr1.length && j < arr2.length) {
+//         if(arr1[i] < arr2[j]){
+//             results.push(arr1[i])
+//             i++;
+//         } else {
+//             results.push(arr2[j])
+//             j++;
+//     }
+// }
+//     while (i < arr1.length) {
+//         results.push(arr1[i]);
+//         i++;
+//     }
+//     while (j < arr2.length){
+//         results.push(arr2[j]);
+//         j++
+//     }
+// return results;
+// }
+
+function merge(arr1, arr2) {
+    const results = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] < arr2[j]) {
+        results.push(arr1[i]);
+        i++;
+      } else {
+        results.push(arr2[j]);
+        j++;
+      }
+    }
+    while (i < arr1.length) {
+      results.push(arr1[i]);
+      i++;
+    }
+    while (j < arr2.length) {
+      results.push(arr2[j]);
+      j++;
+    }
+  
+    return results;
+  }
+
+  function mergeSort (arr){
+    //base case
+    if(arr.length <= 1) return arr;
+    const mid = Math.floor(arr.length/2)
+    const left = mergeSort(arr.slice (0,mid));
+    const right = mergeSort(arr.slice(mid));
+    return merge(left, right)
+    //standard case
+  }
+
+  mergeSort([3,2,70,48])
+        mergeSort([3,2])    mergeSort()
+        mergeSort([3])      mergeSort()
+        [3] [2]
