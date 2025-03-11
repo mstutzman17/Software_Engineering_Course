0ff67c8ec644ccfb841b4ef7e233b0446303a4a5
mstutzman17 <mstutzman17@gmail.com>
2025-03-10 22:15:03 -0400

    Added to work log. Added to Video Curriculm Practice. Added to 11.5_Notes.

--- Unit_11-Intermediate_JavaScript/11.5_Asynchronous_Programming/Video_Curriculum/11.5_practice.js
+++ Unit_11-Intermediate_JavaScript/11.5_Asynchronous_Programming/Video_Curriculum/11.5_practice.js
@@ -1,37 +1,205 @@
-// let promise = fetch(wordnikAPI);
-// promise.then(gotData);
-// promise.catch(gotErr);
-
-// function gotData(data){
-//     console.log(data);
+// const whereIsMyCoffeeOrder = funciton(orderId){
+//     return new Promise((resolve, reject) =>) {
+//         cofffeeApi.checkStatus(orderId, (error, coffeeStatus)) => {
+//             if (error) {
+//                 //Promise fails
+//                 reject(error)
+//             }
+//             else {
+//                 //Promise is fulfilled
+//                 resolve(coffeeStatus)
+//             }
+//         }
+//     }
 // }
 
-// function gotErr(err){
-//     console.log(err);
+// let response =axiosRequest.get('https://www.boredapi.com/api/activity')
+// console.log(`You could ${response.data.activity}`)
+
+// axiosRequest
+//     .get('https://www.boredapi.com/api/activity')
+//     .then(response => {
+//         console.log (`You could ${response.data.activity}`)
+//     })
+//     .catch(error => {
+//         console.log(`ERROR ${error}`)
+//     })
+
+    // https://httpstat.us/404
+
+// const axiosRequest = require('axios')
+
+// async function getActivity() {
+//     try {
+//         let response = await axiosRequest.get('https://www.boredapi.com/api/activity')
+//         console.log (`You could ${response.data.activity}`)
+//     } catch (error) {
+//         console.error(`ERROR ${error}`)
+//     }
 // }
 
-// fetch(wordnikAPI)
-//     .then(function (data){
-//      console.log(data);
+// function promiseA (a) {
+//     return new Promise((resolve, reject ) => {
+//         setTimeout(() => {
+//             if (a == 1) {
+//                 return reject ('a cannot equal to 1')
+//             }
+
+//             resolve (a + a)
+//         }, 1000)
+//     })
 // }
-// ).catch(function (err){
-//     console.log(err);
-// });
 
-// fetch(wordnikAPI)
-//     .then(data => console.log (data))
-//     .catch(err => console.log(err));
+// const STATE = {
+//     PENDING: 'PENDING',
+//     SUCCESS: 'SUCCESS',
+//     FAILED: 'FAILED'
+// }
 
-//     fetch(wordnikAPI)
-//     .then(response => response.json())
-//     .then (json => createP(json.word))
-//     .catch(err => console.log(err));
+// class MyPromise{
+//     constructor(executeFunction) {
+//         this.val = null;
+//         this.state= STATE.PENDING;
+//         this.successfulCallbacks = [];
+//         this.failedCallbacks = [];
+//         try {
+//             executeFunction(
+//                 value => this.resolve(val),
+//                 val => this.reject(val)
+//             )
+//         } catch(error) {
+//             this.reject(error)
+//         }
+//     }
 
-    function setup() {
-        noCanvas();
-        setTimeout(sayHellom, 1000);
-    }
+//     resolve(val) {
+//         this.val = val;
+//         this.state= STATE.SUCCESS;
+//         this.successfulCallbacks.forEach(cb => cb());
+//     }
 
-    function sayHello() {
-        createP('hello');
-    }
+//     reject(val) {
+//         this.val = val;
+//         this.state= STATE.REJECTED;
+//         this.failedCallbacks.forEach(cb => cb());
+
+//     }
+
+//     then(onResolve, onReject) {
+//         return new MyPromise((resolve, reject) => {
+
+//             const successCaller = () => {
+//                 if(!onResolve)return resolve(this.val);
+
+//                 try {
+//                     let val = onResolve(this.val);
+//                     resolve(onResolve);
+//                 } catch(error) {
+//                   reject(error);
+//                 }
+//             }
+
+//             const failedCaller = () => {
+//                 if(!onReject) return reject (this.val);
+
+//                 try {
+//                     let val = onReject(this.val);
+//                     resolve(val);
+//                 } catch(error) {
+//                   reject(error);
+//                 }
+//             }
+
+//             switch(this.state) {
+//                 case STATE.PENDING:
+//                     this.successfulCallbacks.push(successCaller);
+//                     this.failedCallbacks.push(failedCaller);
+//                     break;
+//                 case STATE.SUCCESS:
+//                     successCaller();
+//                     break;
+//                 case STATE.REJECTED:
+//                     faileCaller();
+//                     break;
+//                 default:
+//                     throw new Error('State not defined')
+//             }
+//         })
+//     }
+
+//     catch(onReject) {
+//         return this.then
+//     }
+
+// }
+
+
+// promiseA(1)
+//     .then (res => console.log(res))
+//     .catch (err => console.error (err))
+
+    
+function walkDog(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const dogWalked = true;
+
+            if(dogWalked){
+                resolve('You walked the dog');
+            }
+            else{
+                reject("You DIDN'T walk the dog");
+            }
+        }, 1500)
+    })
+}
+
+function cleanKitchen(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const kitchenCleaned = true;
+
+            if(kitchenCleaned){
+                resolve('You cleaned the kitchen');
+            }
+            else{
+                reject("You DIDN'T clean the kitchen!");
+            }
+        }, 2500)
+    })
+}
+
+function takeOutTrash(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const trashDone = true;
+
+            if(trashDone){
+                resolve('You took out the trash');
+            }
+            else{
+                reject("You DIDN'T take out the trash!");
+            }
+        }, 500)
+    })
+}
+
+
+async function doChores(){
+    const walkDogResult = await walkDog();
+    console.log(walkDogResult);
+
+    const cleanKitchenResult = await cleanKitchen()
+    console.log(cleanKitchenResult)
+
+    const takeOutTrashResult = await takeOutTrash()
+    console.log(takeOutTrashResult)
+
+    console.log("You finished all the chores!")
+}
+
+doChores()
+
